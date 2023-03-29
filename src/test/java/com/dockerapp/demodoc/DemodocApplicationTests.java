fc51d80cffff8704af3f8698b9aae05e7c99e277
zin-lin <zinlinhtun34@gmail.com>
2023-03-29 22:45:19 +0100

    First Docker Integration

---
+++ src/test/java/com/dockerapp/demodoc/DemodocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockerapp.demodoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
